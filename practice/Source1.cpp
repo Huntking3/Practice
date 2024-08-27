c11b13331fddca407685e2623c3dbba7bec63132
Huntking3 <hking14@atu.edu>
2024-08-27 10:43:57 -0500

    github to visual studio

--- practice/Source1.cpp
+++ practice/Source1.cpp
@@ -7,4 +7,7 @@ int main()
 	cout << "Hello World" << endl;
 	cout << "test 1!! test 2!! test 3!!" << endl;
 	cout << "sending a push to github" << endl;
+	cout << "sending a pull to visual studio" << endl;
+
 }
+
